e57af78d28b7bc38b0e720cf69aea010fc52cedc
woodam <rsc3399@nate.com>
2015-06-03 00:06:10 +0900

    no message

--- woodam/resources/common/svg.js
+++ woodam/resources/common/svg.js
@@ -2,7 +2,15 @@ define(['jquery'], function ($){
     var svg = {
         /**
          * 원 형태로 그려주는 메소드
-         * @param object { sx : x시작점, sy : y시작점, radius : 반지름, breadth : 폭(넓이) }
+         * @param object {
+         *     sx : x시작점
+          *    , sy : y시작점
+          *    , radius : 반지름
+          *    , breadth : 폭(넓이)
+          *    , cnt : element총갯수
+          *    , ele : 반복할element
+          *    , par : 반복한 element룰 붙여 넣을 부모
+         *     }
          * @returns undefined
          */
         circlePath : function( set ){
@@ -11,10 +19,33 @@ define(['jquery'], function ($){
                 , sy : 250
                 , radius : 100
                 , angle : 10
+                , cnt : 10
+                , ele : "rect"
+                , par : $("svg")
             }
             var initialSet = set ? $.extend( defaultSet, set ) : defaultSet;
 
+            var cnt = 360/initialSet.angle;
+            var angle = 0;
+
+
+            for( var i = 0; i < initialSet.cnt; i++ ){
+                var dx = initialSet.sx + Math.cos( angle ) * initialSet.radius;
+                var dy = initialSet.sy + Math.sin( angle ) * initialSet.radius;
+
+                console.log( dx )
+
+                var ele = initialSet.par.find( initialSet.ele).eq(i).clone();
+                ele.attr({ cx : dx, cy : dy })
+                ele.appendTo( initialSet.par );
+                angle += cnt;
+
+            }
+
+            initialSet.par.find( initialSet.ele).eq(0).remove();
 
         }
     }
+
+    return svg;
 });
